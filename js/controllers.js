65b26965d02d2de9d9f6b90177860358ab03d758
markrugman <mark@marksveryown.com>
2017-08-10 11:46:28 +0100

    Initial Project

---
+++ js/controllers.js
@@ -0,0 +1,23 @@
+angular.module('app.controllers', [])
+  
+.controller('pageCtrl', ['$scope', '$stateParams', '$window', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $window) {
+    $scope.events = [];
+    var dbRef = firebase.database().ref('events');
+    dbRef.on('value',function(snapshot){
+        angular.forEach(snapshot.val(), function(value, key) {
+            this.push(value);
+        }, $scope.events);
+        console.log(snapshot.val());
+        console.log($scope.events);
+        $scope.$apply();
+    });
+    
+    $scope.goTo = function(link){
+        $window.location.href = link;
+        console.log('Going to:'+link);
+    }
+}])
+ 
